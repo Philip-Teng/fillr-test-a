0c9ac0ea56066948c70107b564a31f118e4f9e00
Chan Hoe Teng (Philip) <philipteng6626@gmail.com>
2017-11-12 16:40:24 +1100

    Completed Extract metadata

--- extract.js
+++ extract.js
@@ -1,14 +1,47 @@
-module.exports.extract = function(window) {
+module.exports.extract = function(window,metadata) {
   
   // Write your solution to Task #2 - Extract Metadata task here
-	var $ = window.$;
-          $("table tr td").each(function(){
-              //print the input's attribute names
-              var findHREF = $(this).find('td:first-child:has(a)');
-              var findInput = $(this).find('td:first-child:has(input)');
-              // console.log(findInput.length);
-              // console.log(findHREF.length);
-               		// console.log($(this).text());
-              
-        });
+  
+  var arrayAll = [];
+  var arraySelect = [];
+  var key;
+  var value;
+  var object = {};
+
+  var $ = window.$;
+  //get everything that is in the table row in the window
+  $("tr td").each(function(){
+  	if($(this).attr("class")!=="navigation-list"){
+		arrayAll.push($(this).text());
+  	}
+  });
+
+  //get the select text
+  $("tr td select").each(function(){
+  	if($(this).attr("name")==="country")
+  	{
+  		arraySelect.push($(this).text());
+  	}
+  });
+
+  //exclude out all the unwanted select text
+  arrayAll = $(arrayAll).not(arraySelect).get();
+
+  //formating the key
+  key = arrayAll.filter(function(v){
+  	return v.trim()!=='';
+  });
+  for (var i = 0; i < key.length; i++){
+	key[i] = key[i].trim();
+  }
+
+  //get the value from metadata
+  value = Object.values(metadata);
+
+  //making an object
+  for (var i = 0; i < key.length; i++){
+  	object[key[i]] = value[i];
+	}
+
+	return object;
 }
